affed6fd068200eca4845bfc74a32a68893cf756
Alejandro <alesantin012@hotmail.com>
2021-02-28 19:20:10 -0300

    mongo

--- internal/database/mongo.go
+++ internal/database/mongo.go
@@ -25,7 +25,8 @@ func NewMongoClient(host string) *Mongo {
 		panic(err)
 	}
 
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	err = client.Connect(ctx)
 
 	if err != nil {
